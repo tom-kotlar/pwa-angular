73300af7014c588a6135c0445f32c661476b1bdf
tom-kotlar <tomaskotlar@yahoo.co.uk>
2021-01-07 15:09:54 +0000

    Ref(film):Fetch Films & small error handling

--- src/app/film/film.component.ts
+++ src/app/film/film.component.ts
@@ -1,7 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
-import { Observable } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { catchError, shareReplay, tap, } from 'rxjs/operators';
+
+
+interface Films {
+  characters: string[];
+  created: string;
+  director: string;
+  edited: string;
+  episode_id: number;
+  opening_crawl: string;
+  planets: string[];
+  producer: string;
+  release_date: string;
+  species: string[];
+  starships: string[];
+  title: string;
+  url: string;
+  vehicles: string[]
+}
+
+
 
 @Component({
   selector: 'app-film',
@@ -10,14 +30,33 @@ import { catchError } from 'rxjs/operators';
 })
 export class FilmComponent implements OnInit {
 
-  films
+  films$: Observable<Films>;
+
+
+  private starWarsUrl = "https://swapi.dev/api/films/1"
+
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.fetchFilms()
   }
+
   fetchFilms() {
-    this.http.get('https://swapi.dev/api/films/1')
-   .subscribe(data => this.films = data)
-}
+    this.films$ = this.http.get<Films>(this.starWarsUrl)
+      .pipe(
+        tap(data => console.log(data)),
+        catchError(this.handleError<Films>()),
+        shareReplay()
+      );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      console.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
+  }
+
+
 }
