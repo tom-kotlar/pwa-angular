0516946af3253f3c4cf156793a5fc69b9bc46281
tom-kotlar <tomaskotlar@yahoo.co.uk>
2021-01-17 02:28:17 +0000

    Feat(app.html):version menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { MatIconModule } from '@angular/material/icon'
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatCardModule } from '@angular/material/card'
+import { MatMenuModule } from '@angular/material/menu';
 
 import { HomeComponent } from './home/home.component'
 import { FilmComponent } from './film/film.component';
@@ -39,7 +40,8 @@ import { FilmComponent } from './film/film.component';
     MatIconModule,
     MatButtonModule,
     MatProgressSpinnerModule,
-    MatCardModule
+    MatCardModule,
+    MatMenuModule,
 
   ],
   providers: [],
