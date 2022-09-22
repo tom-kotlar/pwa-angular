8903f5803313de4aeb6b20be4274d89a44d9fa86
tom-kotlar <t.kotlar@yahoo.com>
2022-09-22 21:08:02 +0100

    Ci(*): v 11

--- src/app/film/film.component.spec.ts
+++ src/app/film/film.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { FilmComponent } from './film.component';
 
@@ -6,7 +6,7 @@ describe('FilmComponent', () => {
   let component: FilmComponent;
   let fixture: ComponentFixture<FilmComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ FilmComponent ]
     })
