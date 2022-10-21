1798bfd43c5ff1d6ee17d00d2b75ff52971bdef9
Tomas Kotlr <tomas.kotlar@red-opal.com>
2022-10-21 02:20:17 +0100

    Ref(films.ts):Breakpoints

--- src/app/model/films.ts
+++ src/app/model/films.ts
@@ -8,3 +8,21 @@ export interface Films {
     info: string;
     trailer: string;
 }
+
+export interface Breakpoints {
+    XSmall: string;
+    Small: string;
+    Medium: string;
+    Large: string;
+    XLarge: string;
+    Handset: string;
+    Tablet: string;
+    Web: string;
+    HandsetPortrait: string;
+    TabletPortrait: string;
+    WebPortrait: string;
+    HandsetLandscape: string;
+    TabletLandscape: string;
+    WebLandscape: string;
+}
+
