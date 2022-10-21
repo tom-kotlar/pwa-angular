b44977f33f2be31105aafacd684eee8797c4870e
tom-kotlar <t.kotlar@yahoo.com>
2022-10-21 02:33:21 +0100

    Ref(home.ts): Clean up

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
-import { Films } from '../model/films';
-import { SwPush } from '@angular/service-worker';
-import { NewslaterService } from '../services/newslater.service';
-import { FilmsService } from '../services/films.service';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-home',
