160df6744290dde00907703b62d94f9ae271ab28
tom-kotlar <tomaskotlar@yahoo.co.uk>
2022-10-23 22:36:26 +0100

    Ref(app-routing): Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { FilmComponent } from './film/film.component';
 import { HomeComponent } from './home/home.component';
 import { MoviesComponent } from './movies/movies.component';
+import { NotFoundComponent } from './shared/not-found.component';
+import { MovieComponent } from './movie/movie.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
   { path: 'movies', component: MoviesComponent }, 
+  { path: 'movie/:id', component: MovieComponent },
+  { path: 'characters', component: FilmComponent }, 
+  { path: '**', component: NotFoundComponent }
+  
   
 
 ];
