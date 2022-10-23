7498b0861cab7a5f909fd67acde742025f969a7e
tom-kotlar <tomaskotlar@yahoo.co.uk>
2022-10-23 22:35:45 +0100

    Build(not-found.ts): NotFoundComponent

---
+++ src/app/shared/not-found.component.ts
@@ -0,0 +1,19 @@
+
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found',
+  template: `
+  
+  <div fxLayout="column" fxLayoutAlign="center center" style="padding-top: 50px"> 
+  <a  mat-button href="home" >BACK</a>
+    <img src="https://miro.medium.com/max/1400/1*ASPrAbd2qtFVSVuru7Y7Rw.png" width="50%"  alt="404 page" style="padding-top: 10px" >
+    <a href="https://dribbble.com/shots/2309412-Daily-UI-404-Error" style="text-decoration: none">Credits</a>
+  </div>
+  
+ 
+    `
+})
+export class NotFoundComponent {}
+
