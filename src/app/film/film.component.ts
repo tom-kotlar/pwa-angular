a6cfe36bb315db02fdc3606ccd0aeef9d176b118
tom-kotlar <tomaskotlar@yahoo.co.uk>
2022-10-27 10:29:38 +0100

    Ref(film.*): Update

--- src/app/film/film.component.ts
+++ src/app/film/film.component.ts
@@ -1,61 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { catchError, shareReplay, tap, } from 'rxjs/operators';
-
-
-interface Films {
-  characters: string[];
-  created: string;
-  director: string;
-  edited: string;
-  episode_id: number;
-  opening_crawl: string;
-  planets: string[];
-  producer: string;
-  release_date: string;
-  species: string[];
-  starships: string[];
-  title: string;
-  url: string;
-  vehicles: string[]
-}
+import { catchError, shareReplay, tap } from 'rxjs/operators';
+import { FilmsService } from '../services/films.service';
 
 
 
 @Component({
   selector: 'app-film',
   templateUrl: './film.component.html',
-  styleUrls: []
+  styleUrls: [],
 })
 export class FilmComponent implements OnInit {
-
-  films$: Observable<Films>;
   
-  private starWarsUrl = "https://swapi.dev/api/films/1"
+  results: any = [];
 
-  constructor(private http: HttpClient) { }
+  totalResults: any;
+  page: number = 1;
+
+  constructor(private http: HttpClient, private filmsService: FilmsService) {}
 
   ngOnInit(): void {
-    this.fetchFilms()
-  }
-
-  fetchFilms() {
-    this.films$ = this.http.get<Films>(this.starWarsUrl)
-      .pipe(
-        tap(data => console.log(data)),
-        catchError(this.handleError<Films>()),
-        shareReplay()
-      );
-  }
-
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-      console.error(error);
-      console.log(`${operation} failed: ${error.message}`);
-      return of(result as T);
-    };
+    this.fetchCharacters();
   }
 
 
+
+  fetchCharacters() {
+    this.filmsService
+      .wikimedia('Star_Wars')
+      .subscribe((data: any) => {
+        console.log(data, "-->");
+        this.results = data.query.search;
+
+      this.totalResults = data.query.search.length;
+      });
+  }
+
+ 
 }
--- src/app/services/films.service.ts
+++ src/app/services/films.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import { Movie } from '../model/interface';
+
 
 
 @Injectable({
@@ -10,11 +11,12 @@ import { Movie } from '../model/interface';
 })
 export class FilmsService {
 
+  wikiURL = "https://en.wikipedia.org/w/api.php"
   constructor(private http: HttpClient) {
 
   }
 
-  loadAllLessons(): Observable<Movie[]> {
+  loadAllLessons(): Observable<any> {
     return this.http.get<any>('/api/films')
       .pipe(
         map(res => res.lessons)
@@ -22,6 +24,27 @@ export class FilmsService {
   }
 
   findLessonById(id: number) {
-    return this.http.get<Movie>('/api/lessons/' + id);
+    return this.http.get<any>('/api/films/')
+    .pipe(
+      tap(data => console.log(data.lessons, id )),
+      map(  
+        
+        data => data.lessons.filter(data => data.id === id)
+      
+      ), 
+    )
+  }
+
+  wikimedia(searText: string | any) {
+    return this.http.get<any>(this.wikiURL, {
+      params: {
+        action: 'query',
+        format: 'json',
+        list: 'search',
+        srsearch: searText,
+        origin: '*',
+        srlimit: 100,
+      }
+    });
   }
 }
