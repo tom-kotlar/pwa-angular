3c6ac9560cb473517660dc235b3fad330abf63d9
tom-kotlar <t.kotlar@yahoo.com>
2022-12-29 17:34:58 +0000

    Build(sanitze.pipe): Display youtube vid

---
+++ src/app/sanitizer.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer } from "@angular/platform-browser";
+
+@Pipe({
+  name: 'sanitizer'
+})
+export class SanitizerPipe implements PipeTransform {
+  constructor(private sanitizer: DomSanitizer) { }
+
+  transform(url): unknown {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url +'?autoplay=1' ) 
+  }
+
+}
