4114a4172c941b511ffd09ac3aafe0f891a925b4
tom-kotlar <t.kotlar@yahoo.com>
2023-01-07 23:25:40 +0000

    Style(not-found): Image

--- src/app/shared/not-found.component.ts
+++ src/app/shared/not-found.component.ts
@@ -6,14 +6,14 @@ import { Component } from '@angular/core';
   selector: 'app-not-found',
   template: `
   
-  <div fxLayout="column" fxLayoutAlign="center center" style="padding-top: 50px"> 
+  <div fxLayout="column" fxLayoutAlign="center center" style="padding-top: 60px"> 
   <a  mat-button href="home" >BACK</a>
-    <img src="https://miro.medium.com/max/1400/1*ASPrAbd2qtFVSVuru7Y7Rw.png" width="50%"  alt="404 page" style="padding-top: 10px" >
+    <img src="https://miro.medium.com/max/1400/1*ASPrAbd2qtFVSVuru7Y7Rw.png" width="90%"  alt="404 page" style="padding-top: 10px" >
     <a href="https://dribbble.com/shots/2309412-Daily-UI-404-Error" style="text-decoration: none">Credits</a>
   </div>
   
  
     `
 })
-export class NotFoundComponent {}
+export class NotFoundComponent { }
 
